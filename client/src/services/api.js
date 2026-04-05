87f964a6aee0f9b8e84b15517eae83ef90dcec44
TheHarshKadam <harshvardhankadam89@gmail.com>
2026-04-05 12:10:55 +0530

    deployed backend in prod

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/game",
+  //baseURL: "http://localhost:5000/api/game",
+  //baseURL: "https://mapora.onrender.com/api/game"
+  baseURL: `${import.meta.env.VITE_API_URL}/api/game`
 });
 
 export const startGameAPI = () => API.post("/start");
